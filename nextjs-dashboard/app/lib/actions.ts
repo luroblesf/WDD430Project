aded23cf7bda6bee950f3c35d638ca3d466d6141
luroblesf <rob23047@byui.edu>
2026-05-20 19:14:54 -0600

    6

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -115,26 +115,16 @@ export async function authenticate(
     formData: FormData,
 ) {
     try {
-        await signIn('credentials', {
-            email: formData.get('email'),
-            password: formData.get('password'),
-            redirectTo: '/dashboard',
-        });
-
+        await signIn('credentials', formData);
     } catch (error) {
-
         if (error instanceof AuthError) {
-
             switch (error.type) {
-
                 case 'CredentialsSignin':
                     return 'Invalid credentials.';
-
                 default:
                     return 'Something went wrong.';
             }
         }
-
         throw error;
     }
 }
--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -23,42 +23,23 @@ export const { auth, signIn, signOut } = NextAuth({
     providers: [
         Credentials({
             async authorize(credentials) {
-
                 const parsedCredentials = z
-                    .object({
-                        email: z.string().email(),
-                        password: z.string().min(6),
-                    })
+                    .object({ email: z.string().email(), password: z.string().min(6) })
                     .safeParse(credentials);
 
                 if (parsedCredentials.success) {
-
                     const { email, password } = parsedCredentials.data;
-
                     const user = await getUser(email);
-
                     if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
 
-                    const passwordsMatch = await bcrypt.compare(
-                        password,
-                        user.password
-                    );
-
-                    if (passwordsMatch) {
-
-                        return {
-                            id: user.id,
-                            name: user.name,
-                            email: user.email,
-                        };
-                    }
+                    if (passwordsMatch) return user;
                 }
 
                 console.log('Invalid credentials');
-
                 return null;
             },
-        })
+        }),
     ],
 });
 
