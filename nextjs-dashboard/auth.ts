72a7c7b2998c6d0eba6b6919481db54f67bef396
luroblesf <rob23047@byui.edu>
2026-05-20 18:50:31 -0600

    5

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -33,7 +33,13 @@ export const { auth, signIn, signOut } = NextAuth({
                     if (!user) return null;
                     const passwordsMatch = await bcrypt.compare(password, user.password);
 
-                    if (passwordsMatch) return user;
+                    if (passwordsMatch) {
+                        return {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email,
+                        };
+                    };
                 }
 
                 console.log('Invalid credentials');
